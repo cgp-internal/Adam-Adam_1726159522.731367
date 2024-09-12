639979824038049005c8161389c6663da056c202
cgp-client <code.gen.pro.system@gmail.com>
2024-09-12 19:45:44 +0300

    Adding frontend/src/index.tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import VacationList from './components/VacationList';
+import VacationDetails from './components/VacationDetails';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
